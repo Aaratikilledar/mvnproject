894f50e97b0cc11f2c50fb5c75faf271fb8071bb
Aaratikilledar <105449573+Aaratikilledar@users.noreply.github.com>
2023-07-06 17:15:11 +0530

    Update MavenprojectApplication.java

--- src/main/java/com/igaptech/mavenproject/MavenprojectApplication.java
+++ src/main/java/com/igaptech/mavenproject/MavenprojectApplication.java
@@ -8,6 +8,7 @@ public class MavenprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenprojectApplication.class, args);
+		System.out.println("hello igap");
 	}
 
 }
